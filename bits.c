19aa04aab474af76d0f8a655eac34d93fc6b81a6
Haobin Cao <haobinca@usc.edu>
2023-09-12 01:06:07 -0700

    Datalab Finished

--- bits.c
+++ bits.c
@@ -3,7 +3,7 @@
  * - Declare variables only at the beginning of a function (as in C89).
  * - Do not include <stdio.h> header (it confuses dlc)
  * - Check correctness with ./btest
- * - Check operator constraints with ./dlc bits.c
+ * - Check operator constraints with ./dlc -e bits.c
  * - Run ./grade before you commit/push your solution
  */
 
@@ -13,8 +13,9 @@
  *   Max ops: 1
  *   Rating: 1
  */
+
 int tmin(void) {
-  return 2;
+  return 0x8 << 28;
 }
 
 /*
@@ -26,7 +27,7 @@ int tmin(void) {
  *   Rating: 1
  */
 int bitOr(int x, int y) {
-  return 2;
+  return ~(~x&~y);
 }
 
 /*
@@ -38,7 +39,7 @@ int bitOr(int x, int y) {
  *   Rating: 1
  */
 int negate(int x) {
-  return 2;
+  return ~x+1;
 }
 
 /*
@@ -50,7 +51,7 @@ int negate(int x) {
  *   Rating: 2
  */
 int isEqual(int x, int y) {
-  return 2;
+  return !(x^y);
 }
 
 /* 
@@ -62,7 +63,9 @@ int isEqual(int x, int y) {
  *   Rating: 4
  */
 int absVal(int x) {
-  return 2;
+  int y;
+  y = x >> 31; //All 1 if neg, All 0 if pos
+  return (y+x) ^ y; //if x was positive, doesn't change the result; if x was negative, add -1 and negate
 }
 
 /*
@@ -73,7 +76,12 @@ int absVal(int x) {
  *   Rating: 3
  */
 int isLess(int x, int y) {
-  return 2;
+  //int sign  = ((!(x>>31)) ^ (!(y>>31)));
+  int xsign, ysign, z;
+  xsign = (x>>31) & 1;
+  ysign = (y>>31) & 1;
+  z = ((~y+x+1)>>31) & 1; //computes the diff between x and y and see if its pos or neg
+  return ((xsign) & (!ysign)) | (z & !(xsign ^ ysign)); //if ysign is 0 and xsign is 1 return 1 immediately; otherwise make sure x and y same sign and return z
 }
 
 /*
@@ -87,7 +95,16 @@ int isLess(int x, int y) {
  *   Rating: 4
  */
 int satAdd(int x, int y) {
-  return 2;
+  int sum, sumSign, xSign, ySign, inputSign, temp;
+  sum = x + y;
+  sumSign = (sum >> 31);
+  xSign = (x >> 31);
+  ySign = (y >> 31);
+  inputSign = ((xSign ^ ySign)); //0 represents same sign
+  inputSign = (inputSign ^ (xSign ^ (sumSign))) & (xSign ^ (sumSign)); //1 if sum and x are diff sign but x and y are same sign, so overflow
+  temp = (~sumSign & (0x8 << 28)) | (sumSign & ((0x8 << 28) ^ (~0x0))); //if sumSign is 1(neg), then return max
+  //return (~inputSign & sum);
+  return (~inputSign & sum) | (inputSign & temp);
 }
 
 /*
@@ -99,7 +116,10 @@ int satAdd(int x, int y) {
  *   Rating: 3
  */
 int dividePower2(int x, int n) {
-    return 2;
+    int sign, y;
+    sign = x >> 31;
+    y = sign & ((1 << n) + ~0x0); //if neg, add 2^n - 1
+    return (x+y) >> n;
 }
 
 /* 
@@ -112,7 +132,9 @@ int dividePower2(int x, int n) {
  *   Rating: 3
  */
 int replaceByte(int x, int n, int c) {
-  return 2;
+    int temp = 0xFF << (n << 3);
+    temp = x & ~temp; //clear the bytes we need to replace
+    return temp + (c << (n << 3));
 }
 
 /*
@@ -127,7 +149,14 @@ int replaceByte(int x, int n, int c) {
  *   Rating: 2
  */
 unsigned floatNegate(unsigned uf) {
- return 2;
+  unsigned temp;
+  temp = (uf >> 23) & 0xFF;
+  if (temp == 0xFF) {
+    if ((uf << 9) != 0x0) {
+      return uf;
+    }
+  } 
+  return uf ^ (0x1 << 31);
 }
 
 /*
@@ -142,7 +171,36 @@ unsigned floatNegate(unsigned uf) {
  *   Rating: 3
  */
 int floatIsEqual(unsigned uf, unsigned ug) {
-    return 2;
+  unsigned ufexpo, uffrac, ugexpo, ugfrac;
+  ufexpo = (uf >> 23) & 0xFF;
+  uffrac = (uf << 9);
+  ugexpo = (ug >> 23) & 0xFF;
+  ugfrac = (ug << 9);
+  if (ufexpo == 0xFF) {
+    if (uffrac != 0x0) {
+      return 0;
+    }
+  }
+  if (ugexpo == 0xFF) {
+    if (ugfrac != 0x0) {
+      return 0;
+    }
+  }
+  if (uf == ug) {
+    return 1;
+  } 
+  if (uffrac == 0x0) {
+    if (ugfrac == 0x0) {
+      if (ufexpo == 0x0) {
+        if (ugfrac == 0x0) {
+          if ((uf << 1) == (ug << 1)) {
+            return 1;
+          }
+        }
+      }
+    }
+  }
+  return 0;
 }
 
 /*
@@ -158,5 +216,32 @@ int floatIsEqual(unsigned uf, unsigned ug) {
  *   Rating: 4
  */
 int floatFloat2Int(unsigned uf) {
-  return 2;
+  unsigned ufexpo, uffrac, temp, sign;
+  ufexpo = (uf >> 23) & 0xFF;
+  temp = 0xFF << 24;
+  sign = uf & (1 << 31);
+  uffrac = (uf & ~temp) | (0x1 << 23);
+  if (ufexpo < 127) {
+    return 0;
+  }
+  else if (ufexpo == 127) {
+    if (sign) {
+      return -1;
+    }
+    else {
+      return 1;
+    }
+  }
+  else if (ufexpo <= 150) {
+    unsigned tempp = (uffrac >> (23 - (ufexpo - 127)));
+    if (sign) {
+      return ~tempp+1;
+    }
+    else {
+      return tempp;
+    }
+  }
+  else {
+    return 0x80000000u;
+  }
 }
